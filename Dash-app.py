b6c15ddeb7a1c09c00c3720debef9015ff1a7f86
SanaeAbm <sanae.abm@gmail.com>
2021-03-26 16:18:02 +0100

    displaying table

--- Dash-app.py
+++ Dash-app.py
@@ -12,10 +12,28 @@ import json
 #Creating app:
 app = dash.Dash(__name__, title="Dash App")
 
-#Loading data:
-col_names=["price","lotsize","bedrooms","bathrooms","stories","driveway","recreation"
-,"fullbase","gasheat","aircon","garage","prefer"] 
-housep_data= pd.read_csv('ag-data.fil', sep="\s+", names=col_names)
+#Loading dataset "House Prices":
+col_names_hp=["price","lotsize","bedrooms","bathrooms","stories","driveway","recreation","fullbase","gasheat","aircon","garage","prefer"] 
+hp_data= pd.read_csv('ag-data.fil', sep="\s+", names=col_names_hp)
+hp_cols = [{"name": i, "id": i} for i in hp_data.columns]
+
+#Data tyding house prices:
+categ_cols_hp=["bedrooms","bathrooms","stories","driveway","recreation","fullbase","gasheat","aircon","garage","prefer"] 
+for col in categ_cols_hp:
+    hp_data[col]= hp_data[col].map(int)
+    hp_data[col]= hp_data[col].astype(object)
+
+
+
+#House prices table:
+table_hp = html.Div([
+    dt.DataTable(id="hp_table",
+        columns = hp_cols,
+        data= hp_data.to_dict("records")
+    )
+])
+
+
 
 #User:
 app.layout = html.Div([
@@ -26,31 +44,40 @@ app.layout = html.Div([
         dcc.Tab(label='2', value='tab-2'),
     ], colors={
         "border": "white",
-        "primary": "Ivory",
+        "primary": "Linen",
         "background": "MediumAquaMarine"
     }),
     html.Div(id='tabs-content-props')
+    
 ])
 
 
 
 
 #Server:
-
 @app.callback(
     Output('tabs-content-props', 'children'),
     Input('tabs-styled-with-props', 'value'))
 def render_content(tab):
     if tab == 'tab-1':
         return html.Div([
-            html.H3('Tab content 1')
+            html.H3('Tab content 1'),
+            table_hp
+    
         ])
     elif tab == 'tab-2':
         return html.Div([
             html.H3('Tab content 2')
         ])
 
+@app.callback(
+     Output('hp_table', 'data'),
+     Input('data', 'children')
+)
 
+def update_table(data,tab):
+        dff = pd.read_json(data)
+        return dff.to_dict("records")
 
 
 
